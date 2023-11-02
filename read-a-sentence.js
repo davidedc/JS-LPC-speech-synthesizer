643e7b2e8af844aaeed55ae743e7b7b327fe6a13
Davide Della Casa <davidedc@gmail.com>
2023-11-02 22:19:29 +0000

    new page that reads a sentence (composed of only 100 selected words)

---
+++ read-a-sentence.js
@@ -0,0 +1,201 @@
+var allWordsLPCModels = [];
+
+class Job {
+    constructor(data) {
+        this.data = data;
+    }
+
+    execute(onSuccess, onError) {
+        throw new Error("Execute method should be implemented by subclasses.");
+    }
+}
+
+class LoadWordDataJob extends Job {
+    execute(onSuccess, onError) {
+        var script = document.createElement('script');
+        script.src = `speechAudio/json-js/${this.data}.mp3.LPC.json.js`;
+
+        // when the script is loaded, first call the "loaded" method in this class, then call the "onSuccess" function
+        script.onload = () => {
+            this.loaded();
+            onSuccess();
+        };
+        script.onerror = onError;
+
+        document.head.appendChild(script);
+    }
+
+    loaded() {
+        console.log(`Loaded script for "${this.data}".`);
+        allWordsLPCModels.push(lpcModelData);
+    }
+}
+
+class JobQueue {
+    constructor() {
+        this.queue = [];
+    }
+
+    enqueue(job) {
+        this.queue.push(job);
+    }
+
+    dequeue() {
+        return this.queue.shift();
+    }
+
+    isEmpty() {
+        return this.queue.length === 0;
+    }
+}
+
+function exception(word) {
+    console.error(`Exception: The script for "${word}" does not exist after 3 attempts.`);
+}
+
+function done() {
+    console.log("All scripts loaded or exceptions handled.");
+    buildPCMForAllWords();
+}
+
+function buildPCMForAllWords() {
+    let jobQueue = new JobQueue();
+
+    allWordsLPCModels.forEach(lpcModelData => {
+        let job = new BuildPCMJob(lpcModelData);
+        jobQueue.enqueue(job);
+    });
+
+    processJobs(jobQueue, playPCMForAllWords);
+}
+
+var pcmSignalBuffers = [];
+
+class BuildPCMJob extends Job {
+    execute(onSuccess, onError) {
+        let pitch = 150;
+        let sampleRate = 1 / this.data.samplingPeriod;
+        let carrierSignalGenerator = new CarrierSignalGenerator(pitch, sampleRate);
+        let pcmSignalBuilder = new LPCtoPCMSignalConverter();
+        pcmSignalBuffers.push(pcmSignalBuilder.build(this.data, pitch, false, carrierSignalGenerator));
+        onSuccess();
+    }
+}
+
+function playPCMForAllWords() {
+    playPCMOfNextWord();
+}
+
+function playPCMOfNextWord() {
+    // play all the pcmSignalBuffers in sequence
+    let audioContext = new AudioContext();
+    let buffer = audioContext.createBuffer(1, pcmSignalBuffers[0].length, 1 / allWordsLPCModels[0].samplingPeriod);
+    let channelData = buffer.getChannelData(0);
+    for (let i = 0; i < pcmSignalBuffers[0].length; ++i) {
+        channelData[i] = pcmSignalBuffers[0][i];
+    }
+    let source = audioContext.createBufferSource();
+    source.buffer = buffer;
+    source.connect(audioContext.destination);
+    source.start();
+
+    source.onended = () => {
+        pcmSignalBuffers.shift();
+        allWordsLPCModels.shift();
+        if (pcmSignalBuffers.length > 0) {
+            playPCMOfNextWord();
+        }
+    }
+}
+
+
+function processJobs(jobQueue, done) {
+    const MAX_ATTEMPTS = 3;
+    let attempts = {};
+
+    function processNext() {
+        if (jobQueue.isEmpty()) {
+            done();
+            return;
+        }
+
+        let job = jobQueue.dequeue();
+        let word = job.data;
+        attempts[word] = (attempts[word] || 0) + 1;
+
+        job.execute(
+            () => {
+                processNext();
+            },
+            () => {
+                if (attempts[word] < MAX_ATTEMPTS) {
+                    console.log(`Retrying to load script for "${word}". Attempt ${attempts[word]}`);
+                    jobQueue.enqueue(job); // Re-add the job to the queue
+                    processNext();
+                } else {
+                    exception(word);
+                    processNext();
+                }
+            }
+        );
+    }
+
+    processNext();
+}
+
+
+// add a dropdown with some sentences.
+let dropdown = document.createElement('select');
+dropdown.id = 'dropdown';
+document.body.appendChild(dropdown);
+
+const sentences = [
+    'one day',
+    'They can make a new day for all people',
+    'People say that only time will tell if he can make a new way for them',
+    'He can also find time to be with her on a day',
+    'People can make a day by giving simple things',
+    'We can all make one day new by our way',
+    'We can all make time for more',
+    'We can make a new day by the way we see and think about people'
+];
+
+sentences.forEach((sentence) => {
+    let option = document.createElement('option');
+    option.value = option.textContent = sentence;
+    dropdown.appendChild(option);
+});
+dropdown.addEventListener('change', function() {
+        textbox.textContent = dropdown.value;
+    }
+);
+
+
+// add a textbox and a play button to the document
+let textbox = document.createElement('textarea');
+textbox.id = 'textbox';
+// pre-populate the textbox with the sentence "one day"
+textbox.textContent = 'one day';
+document.body.appendChild(textbox);
+
+let playButton = document.createElement('button');
+playButton.id = 'playButton';
+playButton.textContent = 'Read';
+document.body.appendChild(playButton);
+
+// when the play button is clicked...
+
+playButton.addEventListener('click', function() {
+
+    // get the words from the textbox
+    let text = document.getElementById('textbox').value;
+    let words = text.split(' ');
+    let jobQueue = new JobQueue();
+
+    words.forEach(word => {
+        let job = new LoadWordDataJob(word);
+        jobQueue.enqueue(job);
+    });
+
+    processJobs(jobQueue, done);
+});
