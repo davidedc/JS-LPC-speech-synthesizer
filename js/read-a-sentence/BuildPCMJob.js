8d5eaae98a7ffa55de14320b947235ba96bfc479
Davide Della Casa <davidedc@gmail.com>
2023-11-09 21:00:54 +0000

    Refactored the pitch values for different types of words in BuildPCMJob.

--- js/read-a-sentence/BuildPCMJob.js
+++ js/read-a-sentence/BuildPCMJob.js
@@ -18,23 +18,23 @@ class BuildPCMJob extends Job {
         // we do this so that "multiple verb words" are not too monotonous
         // e.g. "can make" or "have done" "have gone" "can do" "can go" etc. etc.
         if (this.wordNumber == 0){
-            pitch = 150;
+            pitch = 150; // pitch for the first word
         } else if (this.isVerb) {
-            pitch = 160;
+            pitch = 160; // pitch for a verb
         } else if(this.isNoun) {
-            pitch = 160;
+            pitch = 160; // pitch for a noun
         } else {
-            pitch = 150;
+            pitch = 150; // pitch for a everything else
         }
 
         // if this was the penultimate word, descend the pitch a little
         if (this.isPenuiltimateJob()) {
-            pitch = 140;
+            pitch = 140; // pitch for the penultimate word
         }
 
         // if this was the last word, then end on a lower pitch
         if (this.isLastJob()) {
-            pitch = 125;
+            pitch = 125; // pitch for the last word
         }
 
         console.log("word, verb?, noun?, pitch: ", this.wordToBuild, this.isVerb, this.isNoun, pitch);
